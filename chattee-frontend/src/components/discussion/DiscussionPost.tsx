71c8b50627ac9efa4f72952503c731be016b5204
zrdzn <zrdzn9@gmail.com>
2023-01-24 18:39:47 +0100

    Update discussion posts

---
+++ chattee-frontend/src/components/discussion/DiscussionPost.tsx
@@ -0,0 +1,20 @@
+export const DiscussionPost = ({ description, createdDate, replierSignature, authorName, authorAvatarUrl }: any) => {
+    return (
+        <>
+            <div className="c-card block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden mb-4">
+                <div className="p-3">
+                    <div className="flex items-center">
+                        <span className="text-xs text-gray-400">{createdDate}</span>
+                    </div>
+                </div>
+                <div className="p-4 border-t border-b">
+                    {description}
+                </div>
+                <div className="p-4 flex border-b text-sm space-x-1">
+                    <img src={authorAvatarUrl} alt="..." className="shadow w-5 rounded-full border-none" />
+                    <span>{authorName}</span>
+                </div>
+            </div>
+        </>
+    );
+}
--- chattee-frontend/src/components/discussion/DiscussionView.tsx
+++ chattee-frontend/src/components/discussion/DiscussionView.tsx
@@ -1,3 +1,5 @@
+import {DiscussionPost} from "./DiscussionPost";
+
 export const DiscussionView = ({ id, authorName, title, description, createdDate }: any) => {
     return (
         <>
@@ -8,19 +10,18 @@ export const DiscussionView = ({ id, authorName, title, description, createdDate
                 <span className="flex items-center mb-5">
                     <p className="text-sm text-gray-400">Opened by {authorName}.</p>
                 </span>
-                <a href={"/discussions/" + id} className="c-card block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden">
-                    <div className="p-3">
-                        <div className="flex items-center">
-                            <span className="text-xs text-gray-400">{createdDate}</span>
-                        </div>
-                    </div>
-                    <div className="p-4 border-t border-b">
-                        {description}
-                    </div>
-                    <div className="p-4 border-b text-xs">
-                        Custom user's signature
-                    </div>
-                </a>
+                <DiscussionPost description={description}
+                                createdDate="January 24, 2023"
+                                authorName="zrdzn"
+                                authorAvatarUrl="https://www.spigotmc.org/data/avatars/l/732/732179.jpg?1654355022" />
+                <DiscussionPost description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vestibulum, nunc eu rutrum ornare, neque lorem rhoncus lorem, nec tincidunt libero ligula in erat. Ut non sem elit. Donec vel lacinia magna. Maecenas condimentum, ex eu auctor bibendum, ex libero eleifend nulla, vel auctor risus arcu finibus augue. Aliquam ac odio porttitor nunc volutpat convallis ut ac enim. Vestibulum ultrices metus a lacus sollicitudin, eget tempus elit rutrum. Sed mollis lorem id hendrerit efficitur. Sed id neque vitae quam vulputate vestibulum euismod sit amet erat. Aliquam quis sem vitae eros posuere porttitor. Quisque libero lectus, tristique a lacinia quis, ullamcorper sed sapien. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Ut ullamcorper massa at sapien porta dapibus. Aliquam tempus velit nisl, ut finibus dolor fringilla at. Pellentesque scelerisque tincidunt lorem vel facilisis. Phasellus gravida elit vulputate iaculis lacinia. "
+                                createdDate="January 24, 2023"
+                                authorName="zrdzn"
+                                authorAvatarUrl="https://www.spigotmc.org/data/avatars/l/732/732179.jpg?1654355022" />
+                <DiscussionPost description="Sed condimentum venenatis odio et gravida. Sed id est facilisis, molestie orci eget, condimentum felis. Vivamus nec tristique ante. Suspendisse non porttitor diam. Morbi vel magna non magna commodo suscipit et non massa. Vivamus ac eros augue. Nullam ullamcorper at nunc ac lacinia. Nam mattis enim et nibh porta, eget volutpat est elementum. In hac habitasse platea dictumst. Aliquam volutpat fringilla sem, et luctus diam elementum ut. Fusce quis porta velit. Sed eget cursus lectus, quis rhoncus enim. "
+                                createdDate="January 24, 2023"
+                                authorName="zrdzn"
+                                authorAvatarUrl="https://www.spigotmc.org/data/avatars/l/732/732179.jpg?1654355022" />
             </div>
         </>
     );
