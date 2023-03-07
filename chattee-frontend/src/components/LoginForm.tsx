10935919bab68ec8c62a3a82a44af5a87aca0570
zrdzn <zrdzn9@gmail.com>
2023-03-07 12:12:37 +0100

    Add logic to login form

--- chattee-frontend/src/components/LoginForm.tsx
+++ chattee-frontend/src/components/LoginForm.tsx
@@ -1,12 +1,20 @@
 import { AuthFormHeader } from "./auth/AuthFormHeader";
 import {useState} from "react";
+import { useRouter } from 'next/router'
+import axios from "axios";
 
 export const LoginForm = () => {
     const [credentials, setCredentials] = useState({ email: "", password: "" })
+    const [errorMessage, setErrorMessage] = useState();
+
+    let router = useRouter();
 
     const handleSubmit = async (event: any) => {
         event.preventDefault();
-        //await logIn(credentials);
+
+        axios.post("http://localhost:7070/api/v1/auth", credentials)
+            .then(() => router.push("../"))
+            .catch(error => setErrorMessage(error.response.data.message))
     };
 
     const handleChange = (event: any) => {
@@ -18,17 +26,20 @@ export const LoginForm = () => {
             <div className="flex items-center justify-center h-screen">
                 <div className="min-w-fit flex-col border bg-white px-10 py-14 shadow-md rounded-[4px] ">
                     <AuthFormHeader title="Sign in"></AuthFormHeader>
-                    <form method="post">
+                    <form method="post" onSubmit={handleSubmit}>
                         <div className="flex flex-col text-sm rounded-md">
                             <input className="mb-5 rounded-[4px] border p-3 hover:outline-none focus:outline-none hover:border-gray-300"
                                    type="email"
-                                   name="username"
-                                   placeholder="EMAIL" />
+                                   name="email"
+                                   placeholder="EMAIL"
+                                   onChange={handleChange} />
                             <input className="border rounded-[4px] p-3 hover:outline-none focus:outline-none hover:border-gray-300"
                                    type="password"
                                    name="password"
-                                   placeholder="PASSWORD" />
+                                   placeholder="PASSWORD"
+                                   onChange={handleChange} />
                         </div>
+                        {<div className="mt-5 text-normal text-red-500 ">{errorMessage}</div>}
                         <button className="mt-5 w-full border p-2 bg-gray-800 text-white rounded-[4px] hover:bg-slate-700 scale-105 duration-300"
                                 type="submit">Sign in
                         </button>
