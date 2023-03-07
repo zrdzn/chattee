213394379f105b7fcb75ba97e4f5059ba7ecbdf0
zrdzn <zrdzn9@gmail.com>
2023-03-07 12:25:01 +0100

    Add logic to register form

--- chattee-frontend/src/components/RegisterForm.tsx
+++ chattee-frontend/src/components/RegisterForm.tsx
@@ -1,25 +1,52 @@
 import { AuthFormHeader } from "./auth/AuthFormHeader";
+import {useState} from "react";
+import {useRouter} from "next/router";
+import axios from "axios";
 
 export const RegisterForm = () => {
+    const [credentials, setCredentials] = useState({
+        email: "",
+        password: "",
+        username: ""
+    })
+    const [errorMessage, setErrorMessage] = useState()
+
+    let router = useRouter();
+
+    const handleSubmit = async (event: any) => {
+        event.preventDefault();
+
+        axios.post("http://localhost:7070/api/v1/account/register")
+            .then(() => router.push("../"))
+            .catch(error => setErrorMessage(error))
+    };
+
+    const handleChange = (event: any) => {
+        setCredentials({ ...credentials, [event.target.name]: event.target.value });
+    };
+
     return (
         <>
             <div className="flex items-center justify-center h-screen">
                 <div className="min-w-fit flex-col border bg-white px-10 py-14 shadow-md rounded-[4px] ">
                     <AuthFormHeader title="Sign up"></AuthFormHeader>
-                    <div className="flex flex-col text-sm rounded-md">
-                        <input className="mb-5 rounded-[4px] border p-3 hover:outline-none focus:outline-none hover:border-gray-300"
-                               type="email" placeholder="EMAIL"/>
-                        <input className="mb-5 rounded-[4px] border p-3 hover:outline-none focus:outline-none hover:border-gray-300"
-                               type="username" placeholder="USERNAME"/>
-                        <input className="mb-5 border rounded-[4px] p-3 hover:outline-none focus:outline-none hover:border-gray-300"
-                               type="password" placeholder="PASSWORD"/>
-                        <input className="border rounded-[4px] p-3 hover:outline-none focus:outline-none hover:border-gray-300"
-                               type="password" placeholder="RETYPE PASSWORD"/>
-                    </div>
-                    <button
-                        className="mt-5 w-full border p-2 bg-gray-800 text-white rounded-[4px] hover:bg-slate-700 scale-105 duration-300"
-                        type="submit">Sign up
-                    </button>
+                    <form method="post" onChange={handleChange} onSubmit={handleSubmit}>
+                        <div className="flex flex-col text-sm rounded-md">
+                            <input className="mb-5 rounded-[4px] border p-3 hover:outline-none focus:outline-none hover:border-gray-300"
+                                   type="email" placeholder="EMAIL"/>
+                            <input className="mb-5 rounded-[4px] border p-3 hover:outline-none focus:outline-none hover:border-gray-300"
+                                   type="username" placeholder="USERNAME"/>
+                            <input className="mb-5 border rounded-[4px] p-3 hover:outline-none focus:outline-none hover:border-gray-300"
+                                   type="password" placeholder="PASSWORD"/>
+                            <input className="border rounded-[4px] p-3 hover:outline-none focus:outline-none hover:border-gray-300"
+                                   type="password" placeholder="RETYPE PASSWORD"/>
+                        </div>
+                        {<div className="mt-5 text-normal text-red-500 ">{errorMessage}</div>}
+                        <button
+                            className="mt-5 w-full border p-2 bg-gray-800 text-white rounded-[4px] hover:bg-slate-700 scale-105 duration-300"
+                            type="submit">Sign up
+                        </button>
+                    </form>
                     <div className="mt-5 flex justify-between text-sm text-gray-600">
                         <a href="../account/login">Already have an account?</a>
                     </div>
