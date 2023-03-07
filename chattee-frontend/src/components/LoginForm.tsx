cdd012f1b7404a4042311d498a5ac4ef8d5bba33
zrdzn <zrdzn9@gmail.com>
2023-03-07 12:24:31 +0100

    Move handleChange to form

--- chattee-frontend/src/components/LoginForm.tsx
+++ chattee-frontend/src/components/LoginForm.tsx
@@ -26,18 +26,16 @@ export const LoginForm = () => {
             <div className="flex items-center justify-center h-screen">
                 <div className="min-w-fit flex-col border bg-white px-10 py-14 shadow-md rounded-[4px] ">
                     <AuthFormHeader title="Sign in"></AuthFormHeader>
-                    <form method="post" onSubmit={handleSubmit}>
+                    <form method="post" onChange={handleChange} onSubmit={handleSubmit}>
                         <div className="flex flex-col text-sm rounded-md">
                             <input className="mb-5 rounded-[4px] border p-3 hover:outline-none focus:outline-none hover:border-gray-300"
                                    type="email"
                                    name="email"
-                                   placeholder="EMAIL"
-                                   onChange={handleChange} />
+                                   placeholder="EMAIL" />
                             <input className="border rounded-[4px] p-3 hover:outline-none focus:outline-none hover:border-gray-300"
                                    type="password"
                                    name="password"
-                                   placeholder="PASSWORD"
-                                   onChange={handleChange} />
+                                   placeholder="PASSWORD" />
                         </div>
                         {<div className="mt-5 text-normal text-red-500 ">{errorMessage}</div>}
                         <button className="mt-5 w-full border p-2 bg-gray-800 text-white rounded-[4px] hover:bg-slate-700 scale-105 duration-300"
