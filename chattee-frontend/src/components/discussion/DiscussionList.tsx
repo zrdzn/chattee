cd77ba0f35105f449ffd181e86176297eddafd1d
zrdzn <zrdzn9@gmail.com>
2023-03-07 12:40:28 +0100

    Add some logic to /discussions/

--- chattee-frontend/src/components/discussion/DiscussionList.tsx
+++ chattee-frontend/src/components/discussion/DiscussionList.tsx
@@ -1,28 +1,48 @@
 import {DiscussionItem} from "./DiscussionItem";
+import axios from "axios";
+import {useEffect, useState} from "react";
+import {cookies} from "next/headers";
 
 export const DiscussionList = () => {
+    const [discussions, setDiscussions] = useState([]);
+    const [error, setError] = useState("An error occurred while loading discussions.")
+
+
+    useEffect(() => {
+        axios.get('http://localhost:7070/api/v1/discussions')
+            .then((response) => {
+                setDiscussions(response.data);
+            })
+            .catch((error) => {
+                if (error.response.status === 401) {
+                    setError("You do not have access to view discussions.")
+                    return;
+                }
+                setError(error)
+            });
+    }, []);
+
+    if (error) {
+        return <h1>{error}</h1>
+    }
+
     return (
-        <>
-            <div className="container mx-auto mt-24">
-                <div className="flex flex-wrap">
-                    <DiscussionItem id={1}
-                                    title="Another epic discussion"
-                                    description="Wow what an epic discussion we have here."
-                                    repliesAmount={616}
-                                    authorName="zrdzn"
-                                    createdDate="January 23, 2023"
-                                    lastReplierName="zrdzn"
-                                    lastRepliedDate="January 23, 2023" />
-                    <DiscussionItem id={5}
-                                    title="Very cool title"
-                                    description="The weather today is really sunny."
-                                    repliesAmount={5}
-                                    authorName="zrdzn"
-                                    createdDate="January 20, 2023"
-                                    lastReplierName="zrdzn"
-                                    lastRepliedDate="January 20, 2023" />
-                </div>
+        <div className="container mx-auto mt-24">
+            <div className="flex flex-wrap">
+                {discussions.map((discussion: any) => (
+                    <DiscussionItem
+                        key={discussion.id}
+                        id={discussion.id}
+                        title={discussion.title}
+                        description={discussion.description}
+                        repliesAmount={616}
+                        authorName="zrdzn"
+                        createdDate="January 23, 2023"
+                        lastReplierName="zrdzn"
+                        lastRepliedDate="January 23, 2023"
+                    />
+                ))}
             </div>
-        </>
+        </div>
     );
 }
