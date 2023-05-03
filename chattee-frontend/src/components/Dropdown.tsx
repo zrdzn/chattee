24b905c0b0fb75a9ef3d830f88c43680d7caa1db
zrdzn <zrdzn9@gmail.com>
2023-05-03 17:27:19 +0200

    Change dropdown a little

--- chattee-frontend/src/components/Dropdown.tsx
+++ chattee-frontend/src/components/Dropdown.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {useRouter} from "next/router";
 import axios from "axios";
 import toast from "react-hot-toast";
-import {faRightFromBracket} from "@fortawesome/free-solid-svg-icons";
+import {faRightFromBracket, faGear, faUser} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export const Dropdown = ({ username }: any) => {
@@ -27,9 +27,19 @@ export const Dropdown = ({ username }: any) => {
     return (
         <div className="dropdown">
             <label tabIndex={0} className="btn m-1 text-white text-base">{username}</label>
-            <ul tabIndex={0} className="dropdown-content menu p-4 shadow bg-gray-700 rounded-lg w-52">
+            <ul tabIndex={0} className="dropdown-content menu p-4 shadow bg-gray-50 text-gray-800 rounded-lg w-52 space-y-4">
                 <li>
-                    <a onClick={handleSubmit}>
+                    <a>
+                        <FontAwesomeIcon className="mt-1 mr-1" icon={faUser} /> Profile
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        <FontAwesomeIcon className="mt-1 mr-1" icon={faGear} /> Settings
+                    </a>
+                </li>
+                <li className="border-t border-gray-800">
+                    <a className="mt-2" onClick={handleSubmit}>
                         <FontAwesomeIcon className="mt-1 mr-1" icon={faRightFromBracket} /> Logout
                     </a>
                 </li>
